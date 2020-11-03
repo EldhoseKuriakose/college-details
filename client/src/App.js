64ba9e73257ac0fa055ffa8d14b56b271f742241
Eldhose Kuriakose <eldhosekuriakose96@gmail.com>
2020-11-04 00:41:35 +0530

    college lists

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from './components/Navbar/Navbar.component';
+import CollegeList from './components/CollegeList/CollegeList.component';
 import './App.css';
 
 function App() {
+  const [collegeList, setCollegeList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'http://localhost:8000/college/getAllColleges',
+      );
+
+      setCollegeList(result.data.data);
+    }
+    fetchData();
+  },[]);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar />
+      {
+        collegeList
+        ? <CollegeList collegeList={collegeList} />
+        : ''
+      }
     </div>
   );
 }
